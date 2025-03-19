0336f064d932840b3f366562d7750fb735ffa438
utsahichauhan <utsahichauhan77@gmail.com>
2025-03-19 18:11:47 +0530

    about and navbar

---
+++ src/About.jsx
@@ -0,0 +1,53 @@
+import "react";
+// import Navbar from "./Navbar"; // Import the Navbar component
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const AboutUs = () => {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <div className="container about-section mt-5">
+        <center><h2>Our Mission</h2></center>
+        <p className="lead">
+          We aim to preserve and share the sacred knowledge of Ramayan and
+          Bhagavad Geeta through a modern digital platform.
+        </p>
+      </div>
+      <div className="container team-section mt-5">
+        <center><h2>Meet Our Team</h2></center>
+        <div className="row mt-4">
+          <div className="col-md-4">
+            <div className="card text-center">
+            <img src="/img/user1.jpg" className="card-img-top" alt="Priya Patel" style={{ width: "150px", height: "150px", objectFit: "cover", margin: "auto" }} />
+            <div className="card-body">
+                <h5 className="card-title">Utsahi Chauhan</h5>
+                <p className="card-text">Founder & Developer</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card text-center">
+            <img src="/img/user1.jpg" className="card-img-top" alt="Fulnaj Khatoon" style={{ width: "150px", height: "150px", objectFit: "cover", margin: "auto" }} />
+            <div className="card-body">
+                <h5 className="card-title">Priya Patel</h5>
+                <p className="card-text">Content Creator</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card text-center">
+            <img src="/img/user1.jpg" className="card-img-top" alt="Priya Patel" style={{ width: "150px", height: "150px", objectFit: "cover", margin: "auto" }} />
+            <div className="card-body">
+                <h5 className="card-title">Akshay Karan</h5>
+                <p className="card-text">Graphic Designer</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./Login";
 import Register from "./Register";
+import Navbar from "./Navbar"; 
+import AboutUs from "./About";
 
 function App() {
   return (
     <Router>
+      <Navbar /> 
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
     </Router>
   );
