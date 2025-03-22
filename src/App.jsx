7d0bb87110527489240ad45cc7c69d978908ff56
Akshay175 <119586223+Akshay175@users.noreply.github.com>
2025-03-22 09:56:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Register from "./Register";
 import Navbar from "./Navbar"; 
 import AboutUs from "./About";
 import ContactUs from "./Contact";
+import UserManagement from './UserManagement';
 import Home from './Home'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Router>
       <Navbar /> 
       <Routes>
+        <Route path="/UserManagement" element={<UserManagement />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<AboutUs />} />
